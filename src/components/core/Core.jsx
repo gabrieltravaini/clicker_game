38a80aa4643793001f147c09271ecb75fa5065f3
gabrieltravaini <gabriel.travaini@gmail.com>
2024-11-12 22:01:13 -0300

    created scaffolding and resolved header

---
+++ src/components/core/Core.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Core.css"
+
+const Core = () => {
+    return (
+        <div className="Core-game">
+            <div class="main-icon" onclick="incrementScore()">
+                <img src="src\assets\hardware.png" />
+            </div>
+        </div>
+
+    )
+}
+
+export default Core
