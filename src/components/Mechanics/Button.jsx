e9bd0f3ebde3a93642747cc082cce7e5e6494a9b
Gabriel Travaini <gabriel.travaini@gmail.com>
2024-11-25 19:52:28 -0300

    quebrei tudo

--- src/components/Mechanics/Button.jsx
+++ src/components/Mechanics/Button.jsx
@@ -1,11 +1,20 @@
 import React from "react";
-
+import { useState } from "react";
 
 const Button = ({onClick}) => {
+    let [ img, setImg ] = useState()
+
+    function onDown() {
+        setImg('{transform: scale(0.7)}')
+    }
+    function onUp() {
+        setImg('{transform: scale(0.7)}')
+    }
+
     return(
 
 
-        <img className='object-contain'  style={{ cursor: 'pointer'}} src="src\assets\hardware.png" onClick={onClick} />
+        <img className='object-contain' style={{ cursor: 'pointer', img}}  src={'/hardware.png'} onClick={onClick} onMouseDown={onDown} onMouseUp={onUp} />
 
     )
 
